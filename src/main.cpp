90ca26dba3f93e91d3b64a94c3529be2081f7127
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-17 23:27:21 +0100

    rename width and height to display_%

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,18 @@ constexpr int DISPLAY_HEIGHT = 800;
 };  // namespace Config
 
 int main() {
-    constexpr int width =
+    constexpr int display_width =
         static_cast<int>(Config::DISPLAY_ASPECT_RATIO * Config::DISPLAY_HEIGHT);
-    constexpr int height = Config::DISPLAY_HEIGHT;
+    constexpr int display_height = Config::DISPLAY_HEIGHT;
 
-    std::cout << "P3\n" << width << " " << height << "\n255\n";
+    std::cout << "P3\n" << display_width << " " << display_height << "\n255\n";
 
-    for (int row = 0; row < height; ++row) {
-        std::clog << "Scanlines remaining: " << (height - row) << std::endl;
-        for (int col = 0; col < width; ++col) {
-            const double red = static_cast<double>(row) / (height - 1);
-            const double blue = static_cast<double>(col) / (width - 1);
+    for (int row = 0; row < display_height; ++row) {
+        std::clog << "Scanlines remaining: " << (display_height - row)
+                  << std::endl;
+        for (int col = 0; col < display_width; ++col) {
+            const double red = static_cast<double>(row) / (display_height - 1);
+            const double blue = static_cast<double>(col) / (display_width - 1);
             Color color(red, 0.0, blue);
 
             writeColor(std::cout, color);
