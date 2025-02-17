8cc1aa07a5cd8b6460b2d6fbeabf5656a00ad185
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-17 20:36:33 +0100

    minor refactor

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main() {
         std::clog << "Scanlines remaining: " << (height - y) << std::endl;
         for (int x = 0; x < width; ++x) {
             Color color(x, 0, y);
-            color /= 255.999;
+            color /= size;
 
             writeColor(std::cout, color);
             std::cout << '\n';
