36c61e7b6b55de1bc346224a5cceab32b3dec63c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-17 19:11:43 +0100

    bi gradient

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
 int main() {
-	std::cout << "yo\n";
+    constexpr int size = 256;
+    constexpr int width = size;
+    constexpr int height = size;
+
+    std::cout << "P3\n" << width << " " << height << "\n255\n";
+
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            double r = static_cast<double>(x) / static_cast<double>(width - 1);
+            double g = 0.0;
+            double b = static_cast<double>(y) / static_cast<double>(width - 1);
+
+            int int_r = static_cast<int>(r * 255.9999);
+            int int_g = static_cast<int>(g * 255.9999);
+            int int_b = static_cast<int>(b * 255.9999);
+
+            std::cout << int_r << " " << int_g << " " << int_b << "\n";
+        }
+    }
 }
