72a6d3c344e0fe8e819ea567b8842f4aa3f00d4b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-17 19:15:15 +0100

    add progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,11 @@ int main() {
     std::cout << "P3\n" << width << " " << height << "\n255\n";
 
     for (int y = 0; y < height; ++y) {
+        std::clog << "Scanlines remaining: " << (height - y) << std::endl;
         for (int x = 0; x < width; ++x) {
             double r = static_cast<double>(x) / static_cast<double>(width - 1);
-            double g = 0.0;
-            double b = static_cast<double>(y) / static_cast<double>(width - 1);
+            double g = static_cast<double>(y) / static_cast<double>(width - 1);
+            double b = 0.0;
 
             int int_r = static_cast<int>(r * 255.9999);
             int int_g = static_cast<int>(g * 255.9999);
