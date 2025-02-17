abd0da6bb0decc6ecfffffe21e54a7d198a66f4e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-17 19:17:45 +0100

    init vec3 class

---
+++ src/Vec3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vincent lafouasse on 17/02/2025.
+//
+
+#include "Vec3.hpp"
---
+++ src/Vec3.hpp
@@ -0,0 +1,16 @@
+//
+// Created by vincent lafouasse on 17/02/2025.
+//
+
+#ifndef VEC3_H
+#define VEC3_H
+
+
+
+class Vec3 {
+
+};
+
+
+
+#endif //VEC3_H
