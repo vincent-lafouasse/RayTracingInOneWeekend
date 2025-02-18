8c2b3e94e0d4356469410e3895a113b0275cca37
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-18 16:39:54 +0100

    calculate existence of ray-sphere intersection

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,35 @@ constexpr int DISPLAY_HEIGHT = 800;
 namespace Colors {
 const Color WHITE(1.0, 1.0, 1.0);
 const Color BLUE(0.0, 0.0, 1.0);
+const Color LIGHT_BLUE(0.5, 0.7, 1.0);
 const Color BLACK(0.0, 0.0, 0.0);
+const Color RED(1.0, 0.0, 0.0);
 };  // namespace Colors
 
-Color ray_color(const Ray& r) {
+struct Sphere {
+    Point3 center;
+    double radius;
+
+    bool is_hit(const Ray& r) const {
+        const Vec3 oc = this->center - r.origin();
+        const double a = Vec3::dot(r.direction(), r.direction());
+        const double b = -2.0 * Vec3::dot(oc, r.direction());
+        const double c = Vec3::dot(oc, oc) - this->radius * this->radius;
+        const double discriminant = b * b - 4 * a * c;
+        return discriminant >= 0.0;
+    }
+};
+
+Color ray_color(const Ray& r, const Sphere& s) {
+    if (s.is_hit(r))
+        return Colors::RED;
+
     const Vec3 unit_direction = r.direction().normalized();
 
     // a == 0 when y == -1 -> white
     // a == 1 when y == 1  -> black
     const double a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * Colors::WHITE + a * Colors::BLACK;
+    return (1.0 - a) * Colors::WHITE + a * Colors::LIGHT_BLUE;
 }
 
 int main() {
@@ -49,6 +68,8 @@ int main() {
     const Point3 pixel00 =
         viewport_top_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
+    const Sphere sphere = {.center = Point3(0, 0, -1), .radius = 0.5};
+
     std::cout << "P3\n" << display_width << " " << display_height << "\n255\n";
 
     for (int row = 0; row < display_height; ++row) {
@@ -57,7 +78,7 @@ int main() {
         for (int col = 0; col < display_width; ++col) {
             Point3 pixel = pixel00 + col * pixel_delta_u + row * pixel_delta_v;
             Ray ray(camera, pixel - camera);
-            Color color = ray_color(ray);
+            Color color = ray_color(ray, sphere);
 
             writeColor(std::cout, color);
             std::cout << '\n';
