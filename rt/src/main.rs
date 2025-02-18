0b83c752750e4caf835a07a83cddff29099cc212
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-18 23:35:33 +0100

    blue gradient

--- rt/src/main.rs
+++ rt/src/main.rs
@@ -3,24 +3,54 @@
 mod ray;
 mod vec3;
 
+use crate::ray::Ray;
 use crate::vec3::Color;
+use crate::vec3::Point3;
+use crate::vec3::Vec3;
+
+use cgmath::InnerSpace;
 
 const TARGET_ASPECT_RATIO: f64 = 16.0 / 9.0;
 const TARGET_HEIGHT: u32 = 600;
+
+const WHITE: Color = Color::new(1.0, 1.0, 1.0);
+const LIGHT_BLUE: Color = Color::new(0.5, 0.7, 1.0);
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction.normalize();
+    let a: f64 = 0.5 * (unit_direction.y + 1.0);
+
+    (1.0 - a) * WHITE + a * LIGHT_BLUE
+}
 
 fn main() {
     let display_height: u32 = TARGET_HEIGHT;
     let display_width: u32 = (TARGET_HEIGHT as f64 * TARGET_ASPECT_RATIO) as u32;
     write_ppm_header(display_width, display_height);
 
+    let viewport_height: f64 = 2.0; // arbitrary
+    let viewport_width: f64 = viewport_height * display_width as f64 / display_height as f64;
+
+    let camera = Point3::new(0.0, 0.0, 0.0);
+    let fov: f64 = 1.0;
+
+    let viewport_center = camera + Point3::new(0.0, 0.0, -fov);
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / display_width as f64;
+    let pixel_delta_v = viewport_v / display_height as f64;
+
+    let viewport_top_left = viewport_center - 0.5 * (viewport_u + viewport_v);
+    let pixel00 = viewport_top_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
     for row in 0..display_height {
         log_progress(row as f32 / (display_height as f32 - 1.0));
         for col in 0..display_width {
-            let red: f64 = row as f64 / (display_width as f64 - 1.0);
-            let green: f64 = 0.0;
-            let blue: f64 = col as f64 / (display_height as f64 - 1.0);
+            let pixel = pixel00 + col as f64 * pixel_delta_u + row as f64 * pixel_delta_v;
+            let ray = Ray::new(camera, pixel - camera);
 
-            let color = Color::new(red, green, blue);
+            let color = ray_color(&ray);
             write_color(&color);
         }
     }
