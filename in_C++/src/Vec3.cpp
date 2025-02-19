ae7b60f5e9da902f2609ef316442b3360c0c3a3c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-19 12:12:46 +0100

    refactor using Vec3 accessors

--- in_C++/src/Vec3.cpp
+++ in_C++/src/Vec3.cpp
@@ -8,13 +8,13 @@ Vec3::Vec3(const double x, const double y, const double z)
 // TODO: complete rule of 5
 
 double Vec3::x() const {
-    return x_coordinate;
+    return this->x_coordinate;
 }
 double Vec3::y() const {
-    return y_coordinate;
+    return this->y_coordinate;
 }
 double Vec3::z() const {
-    return z_coordinate;
+    return this->z_coordinate;
 }
 double& Vec3::x() {
     return this->x_coordinate;
@@ -72,7 +72,7 @@ double& Vec3::operator[](const std::size_t i) {
 }
 
 Vec3 Vec3::operator+(const Vec3& v) const {
-    return Vec3(x_coordinate + v.x_coordinate, y_coordinate + v.y_coordinate, z_coordinate + v.z_coordinate);
+    return Vec3(this->x() + v.x(), this->y() + v.y(), this->z() + v.z());
 }
 
 Vec3 Vec3::operator-(const Vec3& v) const {
@@ -80,11 +80,11 @@ Vec3 Vec3::operator-(const Vec3& v) const {
 }
 
 Vec3 Vec3::operator-() const {
-    return Vec3(-x_coordinate, -y_coordinate, -z_coordinate);
+    return Vec3(-this->x(), -this->y(), -this->z());
 }
 
 Vec3 Vec3::operator*(const double s) const {
-    return Vec3(x_coordinate * s, y_coordinate * s, z_coordinate * s);
+    return Vec3(this->x() * s, this->y() * s, this->z() * s);
 }
 
 Vec3 Vec3::operator/(const double s) const {
@@ -103,23 +103,23 @@ Vec3 operator/(const double s, const Vec3& v) {
 }
 
 Vec3& Vec3::operator+=(const Vec3& v) {
-    this->x_coordinate += v.x_coordinate;
-    this->y_coordinate += v.y_coordinate;
-    this->z_coordinate += v.z_coordinate;
+    this->x() += v.x();
+    this->y() += v.y();
+    this->z() += v.z();
     return *this;
 }
 
 Vec3& Vec3::operator-=(const Vec3& v) {
-    this->x_coordinate -= v.x_coordinate;
-    this->y_coordinate -= v.y_coordinate;
-    this->z_coordinate -= v.z_coordinate;
+    this->x() -= v.x();
+    this->y() -= v.y();
+    this->z() -= v.z();
     return *this;
 }
 
 Vec3& Vec3::operator*=(const double s) {
-    this->x_coordinate *= s;
-    this->y_coordinate *= s;
-    this->z_coordinate *= s;
+    this->x() *= s;
+    this->y() *= s;
+    this->z() *= s;
     return *this;
 }
 
@@ -128,23 +128,23 @@ Vec3& Vec3::operator/=(const double s) {
         throw Vec3::DivisionByZeroException();
     }
 
-    this->x_coordinate /= s;
-    this->y_coordinate /= s;
-    this->z_coordinate /= s;
+    this->x() /= s;
+    this->y() /= s;
+    this->z() /= s;
     return *this;
 }
 
 Vec3& Vec3::operator+=(const double s) {
-    this->x_coordinate += s;
-    this->y_coordinate += s;
-    this->z_coordinate += s;
+    this->x() += s;
+    this->y() += s;
+    this->z() += s;
     return *this;
 }
 
 Vec3& Vec3::operator-=(const double s) {
-    this->x_coordinate -= s;
-    this->y_coordinate -= s;
-    this->z_coordinate -= s;
+    this->x() -= s;
+    this->y() -= s;
+    this->z() -= s;
     return *this;
 }
 
@@ -153,7 +153,7 @@ double Vec3::magnitude() const {
 }
 
 double Vec3::dot(const Vec3& v) const {
-    return this->x_coordinate * v.x_coordinate + this->y_coordinate * v.y_coordinate + this->z_coordinate * v.z_coordinate;
+    return this->x() * v.x() + this->y() * v.y() + this->z() * v.z();
 }
 
 double Vec3::dot(const Vec3& v1, const Vec3& v2) {
@@ -187,7 +187,7 @@ Vec3 Vec3::normalized() const {
 
 // uses float comparison, bad ?
 bool Vec3::operator==(const Vec3& v) const {
-    return x_coordinate == v.x_coordinate && y_coordinate == v.y_coordinate && z_coordinate == v.z_coordinate;
+    return this->x() == v.x() && this->y() == v.y() && this->z() == v.z();
 }
 
 bool Vec3::operator!=(const Vec3& v) const {
