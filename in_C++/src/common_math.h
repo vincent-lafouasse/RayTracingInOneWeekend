4c276864e3a38cb7ff52f20b5f16d9125860dd30
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-21 14:19:43 +0100

    introduce small math module

---
+++ in_C++/src/common_math.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cmath>
+#include <limits>
+
+constexpr double infinity = std::numeric_limits<double>::infinity();
+constexpr double pi = 3.14159265358979323846;
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
