998961285586317751eb95c5ebafa50f4749d006
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-02-21 15:08:51 +0100

    move viewport logic into Camera

--- in_C++/src/Camera.cpp
+++ in_C++/src/Camera.cpp
@@ -2,6 +2,29 @@
 
 #include "Color.hpp"
 
+Camera::Camera(const uint32_t width, const double aspect_ratio)
+    : width(width),
+      height(static_cast<uint32_t>(width / aspect_ratio)),
+      eye(Point3(0, 0, 0)),
+      fov(1.0) {
+    const double effective_aspect_ratio = width / static_cast<double>(height);
+    const double viewport_height = 2.0;
+    const double viewport_width = viewport_height * effective_aspect_ratio;
+
+    // notice the viewport is orthogonal to Oz
+    const Vec3 viewport_u(viewport_width, 0, 0);
+    const Vec3 viewport_v(0, -viewport_height, 0);
+
+    this->delta_u = viewport_u / this->width;
+    this->delta_v = viewport_v / this->height;
+
+    // notice we look along the negative z-axis
+    const Point3 viewport_center = this->eye + Vec3(0, 0, -this->fov);
+    const Point3 viewport_top_left =
+        viewport_center - viewport_u / 2 - viewport_v / 2;
+    this->pixel00 = viewport_top_left + 0.5 * (this->delta_u + this->delta_v);
+}
+
 void Camera::render(const Hittable& scene) const {
     (void)this;
     (void)scene;
--- in_C++/src/Camera.hpp
+++ in_C++/src/Camera.hpp
@@ -2,11 +2,20 @@
 
 #include "Color.hpp"
 #include "Hittable.hpp"
+#include "Vec3.hpp"
 
 class Camera final {
    public:
+    Camera(uint32_t width, double aspect_ratio);
     void render(const Hittable& scene) const;
 
    private:
     static Color ray_color(const Ray& r, const Hittable& scene);
+
+    uint32_t width, height;
+    Point3 eye;
+    double fov;
+    Point3 pixel00;
+    Vec3 delta_u;
+    Vec3 delta_v;
 };
